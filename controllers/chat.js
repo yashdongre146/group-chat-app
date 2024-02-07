1f6823758338be5943b93ef925212897e8231ec4
Yash Dongre <yash.dongre146@gmail.com>
2024-02-07 14:28:50 +0530

    Optimization scroll feature

--- controllers/chat.js
+++ controllers/chat.js
@@ -1,18 +1,44 @@
 const Chat = require("../models/chat")
 
-exports.storeChat =  (req, res) => {
+exports.storeChat =  async (req, res) => {
+    const limit = 10;
+    const page = parseInt(req.query.page);
     try {
-         req.user.createChat(req.body);
-         res.status(201).json({success: true, message: "sent"})
+         await req.user.createChat(req.body);
+         const count = await Chat.count();
+         const hasMoreData = count - (page-1)*limit > limit ? true : false;
+         res.status(201).json({success: true, message: "sent", hasMoreData})
      } catch (err) {
+            console.log(err);
              res.status(400).json();
      }
  }
 
 exports.getChats =  async (req, res) => {
+    const page = parseInt(req.query.page);
+    const limit = 10;
+    const offset = (page - 1) * limit;
     try {
-         const chats = await Chat.findAll();
-         res.status(201).json(chats)
+         const chats = await Chat.findAll({
+            // order: [['createdAt', 'DESC']],
+            offset: offset,
+            limit: limit
+         });
+         const count = await Chat.count();
+
+         const hasMoreData = count - (page-1)*limit > limit ? true : false;
+         const nextPage = hasMoreData ? Number(page) + 1 : undefined;
+         const previousPage = page > 1 ? Number(page)-1 : undefined;
+         const hasPreviousPage = previousPage ? true : false;
+         res.status(201).json(
+            {
+                 chats: chats,
+                 currentPage: page,
+                 hasNextPage: hasMoreData,
+                 hasPreviousPage: hasPreviousPage,
+                 previousPage: previousPage,
+                 nextPage: nextPage
+            })
      } catch (err) {
              res.status(400).json();
      }
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,11 @@
 const msg = document.getElementById('msg');
 const container = document.querySelector('.container');
 const token = localStorage.getItem('token');
+let currentPage = 1;
+let pageData;
+let prevData;
+let isGetChatsRunning = false;
+let newMessage = false;
 
 function parseJwt (token) {
     var base64Url = token.split('.')[1];
@@ -13,19 +18,65 @@ function parseJwt (token) {
 
 window.addEventListener('DOMContentLoaded', async () => {
     try {
-        getChats();
+        getChats(currentPage);
     } catch (err) {
         console.log(err);
     }
 })
 
-async function getChats(){
+async function getChats(page){
     try {
-        const res = await axios.get(`/getChats`, {headers: {'auth': token}});
-        const decodedToken = parseJwt(token);
+        if (isGetChatsRunning) {
+            return;
+        }
+        isGetChatsRunning = true;
+        const res = await axios.get(`/getChats?page=${page}`, {headers: {'auth': token}});
+        
+        if (_.isEqual(prevData, res.data)) {
+            return;
+        } else {
+            if (newMessage) {
+                const decodedToken = parseJwt(token);
+                currentPage = res.data.currentPage;
+                if (res.data.chats[res.data.chats.length - 1].userId === decodedToken.id) {
+                    const div = document.createElement('div')
+                    div.classList.add('message')
+                    div.classList.add('right')
+                    div.appendChild(document.createTextNode(`${res.data.chats[res.data.chats.length - 1].message}`))
+                    container.appendChild(div)
+                } else {
+                    const div = document.createElement('div')
+                    div.classList.add('message')
+                    div.classList.add('left')
+                    div.appendChild(document.createTextNode(`${res.data.chats[res.data.chats.length - 1].message}`))
+                    container.appendChild(div)
+                } 
+                newMessage = false;
+                prevData = res.data;
+                return;
+            }
+            // if ((prevData && prevData.chats) && (res.data && res.data.chats)){
+            //     //edge case
+            //     if (_.isEqual(res.data.chats[8].message, prevData.chats[7].message)) {
+            //         alert("edge case");
+            //         // Extract the last element of the arra
+            //         const lastItem = res.data.chats[res.data.chats.length - 1];
+                    
+            //         // Modify the original array to contain only the last element
+            //         res.data.chats.length = 0;  // Clear the original array
+            //         res.data.chats.push(lastItem);  // Push the last item back inthe original array
+            //         prevData.chats.push(lastItem);  // Push the last item back inthe original array
 
-        container.innerHTML = ''
-        for(let userChatDetails of res.data){
+            //     }
+            // }
+           
+            // if (!(res.data.chats.length === 1)) {
+                prevData = res.data
+            // }
+        }
+        const decodedToken = parseJwt(token);
+        currentPage = res.data.currentPage;
+        for(let userChatDetails of res.data.chats){
             if (userChatDetails.userId === decodedToken.id) {
                 const div = document.createElement('div')
                 div.classList.add('message')
@@ -40,21 +91,53 @@ async function getChats(){
                 container.appendChild(div)
             }
         }
+        pageData = res.data;
+        // container.scrollTop = container.scrollHeight;
     } catch (err) {
         console.log(err);
+    } finally {
+        // Set the flag to indicate that getChats has completed its execution
+        isGetChatsRunning = false;
     }
 }
 async function storeChat(e){
     e.preventDefault();
     try {
+        isGetChatsRunning = true;
         const userMessage = {
             message: msg.value
         }
-        await axios.post(`/storeChat`,userMessage, {headers: {'auth': token}});
+        const res = await axios.post(`/storeChat?page=${currentPage}`,userMessage, {headers: {'auth': token}});
         msg.value = ''
+        newMessage = true;
+        if (res.data.hasMoreData) {
+            currentPage+=1;
+        }
+        
     } catch (err) {
         console.log(err);
+    } finally{
+        // Set the flag to indicate that getChats has completed its execution
+        isGetChatsRunning = false;
     }
 }
 
-setInterval(getChats, 1000);
+function handleScroll() {
+    var scrollableContainer = document.querySelector(".container");
+
+    // Check if the scrollbar has reached the bottom
+    if (scrollableContainer.scrollTop + scrollableContainer.clientHeight >= scrollableContainer.scrollHeight) {
+        showPagignation(pageData)
+        // Call your function here
+        // Call your function to load more messages or perform some action
+    }
+}
+
+  async function showPagignation(pageData) {
+    if (pageData.hasNextPage) {
+      getChats(pageData.nextPage);
+    }
+  }
+setInterval(()=>{
+    getChats(currentPage)
+}, 1000);
